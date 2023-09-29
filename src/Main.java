1f2ca1e9b6e575f41a28dafdcb7b165f506f377a
unknown <heodaikadz2k3@gmail.com>
2023-09-29 09:38:16 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import controller.EquationManagement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        String title = "=============== Equation program ================";
+        String[] s = new String[]{"Calculate Superlative Equation", "Calculate Quadratic Equation","Exit"};
+        EquationManagement manage = new EquationManagement(title, s);
+        manage.run();
+    }
+}
